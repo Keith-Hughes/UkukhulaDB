5f3384312a2be86739090efc9ecb27776aa4dc2d
Keith Hughes <mrkphughes@gmail.com>
2024-02-28 16:57:00 +0200

    added new procedure. Get Student Request by university

--- procedure.sql
+++ procedure.sql
@@ -108,9 +108,11 @@ BEGIN
     -- Insert the new request into the UniversityFundRequest table
     INSERT INTO UniversityFundRequest (UniversityID, Amount, DateCreated, Comment, StatusID)
     VALUES (@UniversityID, @Amount, GETDATE(), @Comment, 3);
-
+	DECLARE @RequestID INT;
+	SET @RequestID = SCOPE_IDENTITY();
     -- Return the inserted data
     SELECT 
+	@RequestID AS RequestID,
         University.[Name] AS University, 
         Provinces.ProvinceName AS Province, 
         UniversityFundRequest.Amount,
@@ -177,3 +179,44 @@ BEGIN
 
     RETURN @TotalBudget;
 END;
+
+CREATE PROCEDURE [dbo].[GetStudentFundRequestsByUniversity]
+@UniversitID INT
+AS
+BEGIN
+    SELECT
+        [StudentFundRequest].[ID],
+        [User].[FirstName],
+        [User].[LastName],
+        [University].[Name] AS UniversityName,
+        [Student].[IDNumber],
+        [Student].[BirthDate],
+        [Student].[Age],
+        [Gender].[GenderName],
+        [ContactDetails].[Email],
+        [ContactDetails].[PhoneNumber],
+        [Race].[RaceName],
+        [StudentFundRequest].[Grade],
+        [StudentFundRequest].[Amount],
+        [StudentFundRequest].[CreatedDate] AS RequestCreatedDate,
+        [Status].[Type] AS FundRequestStatus,
+        [StudentFundRequest].[Comment],
+        CASE
+            WHEN [Document].[ID] IS NOT NULL THEN 'Received'
+            ELSE 'Pending Document'
+        END AS DocumentStatus
+    FROM
+        [dbo].[StudentFundRequest]
+    INNER JOIN [dbo].[Student] ON [StudentFundRequest].[StudentID] = [Student].[ID]
+    INNER JOIN [dbo].[User] ON [Student].[UserID] = [User].[ID]
+    INNER JOIN [dbo].[ContactDetails] ON [User].[ContactID] = [ContactDetails].[ID]
+    INNER JOIN [dbo].[Gender] ON [Student].[GenderID] = [Gender].[ID]
+    INNER JOIN [dbo].[Race] ON [Student].[RaceID] = [Race].[ID]
+    INNER JOIN [dbo].[UniversityStudentInformation] ON [StudentFundRequest].[StudentID] = [UniversityStudentInformation].[StudentID]
+    INNER JOIN [dbo].[University] ON [UniversityStudentInformation].[UniversityID] = [University].[ID]
+    INNER JOIN [dbo].[Status] ON [StudentFundRequest].[StatusID] = [Status].[ID]
+    LEFT JOIN [dbo].[Document] ON [StudentFundRequest].[ID] = [Document].[RequestID]
+	WHERE [University].[ID] = @UniversitID
+END;
+
+GO
